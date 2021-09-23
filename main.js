7de397e505870e828070c7cdcd22c47e661901ad
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2021-09-23 12:01:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+var camera = document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function takeSnapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_img" src="'+data_uri+'"/>'
+    })
+}
+
+console.log("ml5 version: ", ml5.version);
+var classifier = ml5.classifier('https://teachablemachine.withgoogle.com/models/h0aesD_yW/model.json',modelLoaded());
+
+function modelLoaded() {
+    console.log("model Loaded");
+}
